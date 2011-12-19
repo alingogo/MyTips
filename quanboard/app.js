3573955d881bd165c9dfbcfd90485f47c5869964
alingogo <alin.tottori@gmail.com>
2011-12-19 10:31:03 +0900

    create trunk

---
+++ quanboard/app.js
@@ -0,0 +1,244 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , io = require('socket.io')
+  , mongoose = require('mongoose')
+
+var app = module.exports = express.createServer();
+var mongoUri = 'http://127.0.0.1:27017/qboards';
+var Schema = mongoose.Schema;
+var commentschema = new Schema({
+    id     :Number,
+    text   :String,
+    pos_x  :String,
+    pos_y  :String,
+    width  :String,
+    height :String
+});
+
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+  mongoose.connect(mongoUri);
+});
+var Comment = mongoose.model('Comment', commentschema);
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+//app.get('/', routes.index);
+app.get('/', function(req, res){
+  Comment.find({}, function(err, comments){
+    res.render('index', {title: 'quanBoard',comments: comments});
+  });
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+// process
+io = io.listen(app)
+io.sockets.on('connection', function(socket){
+
+  // 新規
+  socket.on('created', function(data){
+    console.log(data);
+    var mydata = data.my;
+    var comment = new Comment();
+    comment.id     = mydata.id;
+    comment.text   = mydata.text;
+    comment.pos_x  = mydata.pos_x;
+    comment.pos_y  = mydata.pos_y;
+    comment.width  = mydata.width;
+    comment.height = mydata.height;
+    console.log(comment.pos_y);
+    comment.save(function(err){
+      if (!err) console.log('Success!');
+    });
+
+    socket.broadcast.emit('b_created', {my: mydata});
+  });
+
+  // 移動
+  socket.on('moved', function(data){
+    var mydata = data.my;
+    Comment.find({id : mydata.id}, function (err, comment) {
+      if (!err && comment) {
+        Comment.update( { id: mydata.id }, 
+                        { $set: { pos_x: mydata.pos_x,
+                                  pos_y: mydata.pos_y
+                                }}, 
+                        { upsert: false, multi: true }, 
+                        function(err) {}
+                      );
+      } else {
+        console.log(err);
+      }
+    });
+
+    socket.broadcast.emit('b_moved', {my: mydata});   
+  });
+
+  socket.on('resized', function(data){
+    var mydata = data.my;
+    Comment.find({id : mydata.id}, function (err, comment) {
+      if (!err && comment) {
+        Comment.update( { id: mydata.id }, 
+                        { $set: { width: mydata.width,
+                                  height: mydata.height
+                                }}, 
+                        { upsert: false, multi: true }, 
+                        function(err) {}
+                      );
+      } else {
+        console.log(err);
+      }
+    });
+
+    socket.broadcast.emit('b_resized', {my: mydata});   
+  });
+
+  // 編集
+  socket.on('edited', function(data){
+    var mydata = data.my;
+    console.log(data);
+    Comment.find({id : mydata.id}, function (err, comment) {
+      if (!err && comment.length != 0) {
+        Comment.update( { id: mydata.id }, 
+                        { $set: {text: mydata.text,
+                                }}, 
+                        { upsert: false, multi: true }, 
+                        function(err) {if(err){console.log("errrrrrrrrrrrrr");}}
+                      );
+      } else {
+    var comment = new Comment();
+        comment.id     = mydata.id;
+        comment.text   = mydata.text;
+        comment.pos_x  = mydata.pos_x;
+        comment.pos_y  = mydata.pos_y;
+        comment.width  = mydata.width;
+        comment.height = mydata.height;
+        console.log(comment);
+        comment.save(function(err){
+          if (!err) console.log('Success!');
+        });
+      }
+    });
+
+    socket.broadcast.emit('b_edited', {my: mydata});   
+  });
+
+  // 削除
+  socket.on('deleted', function(data){
+    var mydata = data.my;
+    Comment.find({id : mydata.id}, function (err, comment) {
+      if (!err && comment) {
+        Comment.remove( { id: mydata.id }, 
+                        function(err) {}
+                      );
+      } else {
+        console.log(err);
+      }
+    });
+
+    socket.broadcast.emit('b_deleted', {my: mydata});  
+  });
+
+  // ppt
+  socket.on('showme', function(c){
+    var current = {};
+    var cnote = null;
+    var note = {};
+    if ( c.current == '-1' ) {
+      current.pos_x = -1;
+      cnote = null;    
+    Comment.find({}, function (err, comments) {
+      if (!err && comments) {
+        comments.sort(function(a, b){
+          return parseInt(a.pos_x) - parseInt(b.pos_x)
+        });
+        for (i=0;i<comments.length;i++){
+          if ( comments[i].pos_x > current.pos_x ) {
+            note = comments[i];
+//console.log(note);
+            break;   
+          }
+        }
+      } else {
+        console.log(err);
+      }
+
+      socket.emit('next', {current: cnote, next: note});
+      socket.broadcast.emit('next', {current: cnote, next: note});
+    });
+    } else {
+      Comment.find({id: c.current}, function(e, com){
+        var cnote = com;
+        var note = com;
+        Comment.find({}, function (err, comments) {
+          if (!err && comments) {
+            comments.sort(function(a, b){
+              return parseInt(a.pos_x) - parseInt(b.pos_x)
+            });
+            for (i=0;i<comments.length;i++){
+console.log(i);
+console.log(parseInt(comments[i].pos_x));
+console.log(cnote);
+              if ( parseInt(comments[i].pos_x) > parseInt(cnote[0].pos_x) ) {
+                note = comments[i];
+console.log("aaa.............");
+                break;   
+              }
+            }
+          } else {
+            console.log(err);
+          }
+
+          socket.emit('next', {current: cnote, next: note});
+          socket.broadcast.emit('next', {current: cnote, next: note});
+        });
+      });
+    }
+/*
+check_send = function(cnote, note){
+    Comment.find({}, function (err, comments) {
+      if (!err && comments) {
+        comments.sort(function(a, b){
+          return parseInt(a.pos_x) - parseInt(b.pos_x)
+        });
+        for (i=0;i<comments.length;i++){
+          if ( comments[i].pos_x > current.pos_x ) {
+            note = comments[i];
+console.log(note);
+            break;   
+          }
+        }
+      } else {
+        console.log(err);
+      }
+
+      socket.emit('next', {current: cnote, next: note});
+      socket.broadcast.emit('next', {current: cnote, next: note});
+    });
+}
+*/
+  });
+});
---
+++ quanboard/public/javascripts/index.js
@@ -0,0 +1,208 @@
+var newNote = {};
+var socket = io.connect();
+var new_note_id = "";
+jQuery(function(){
+  jQuery.fn.stickyNotes.tempNote = function(note){
+    var pos_x = 100;
+    var pos_y = 760;
+    var note_id = (new Date).getTime().toString();
+    new_note_id = note_id;
+
+    var _note_content = jQuery(document.createElement('textarea'));
+    var _div_note   =       jQuery(document.createElement('div')).addClass('jStickyNote');
+    var _div_background = jQuery.fn.stickyNotes.createNoteBackground();
+
+    _div_note.append(_note_content);
+    var _div_delete =       $(document.createElement('div'))
+                                              .addClass('jSticky-delete')
+                                              .click(function(){jQuery.fn.stickyNotes.deleteNote(this);});
+    var _div_wrap   =       $(document.createElement('div'))
+                                        .css({'position':'absolute','top':pos_x,'left':pos_y, 'float' : 'left'})
+                                        .attr("id", "note-" + note_id)
+                                        .append(_div_background)
+                                        .append(_div_note)
+                                        .append(_div_delete);
+    _div_wrap.addClass('jSticky-medium');
+    _div_wrap.draggable({containment: '#sticky-container', scroll: false, stop: function(event, ui){jQuery.fn.stickyNotes.movedNote(note_id)}});
+
+
+    $('#model').append(_div_wrap);
+    jQuery("#note-" + note_id).click(function() {
+      return false;
+    })
+    var note = {"id":note_id,
+                "text":"",
+                "pos_x": pos_x,
+                "pos_y": pos_y,
+                "width": jQuery(_div_wrap).width(),
+                "height": jQuery(_div_wrap).height()};
+    jQuery(_note_content).css({
+              'width': jQuery("#note-" + note_id).width() - 44,
+              'height': jQuery("#note-" + note_id).height() - 15
+    });
+  }
+});
+
+var stopEdit = function() {
+                 text = $("#note-" + new_note_id).find('textarea').val();
+                 var _p_note_text =      $(document.createElement('p')).attr("id", "p-note-" + new_note_id)
+                                                                       .html("<pre>" + text + "</pre>");
+                 $("#note-" + new_note_id).find('textarea').replaceWith(_p_note_text);
+                 $("#p-note-" + new_note_id).dblclick(function() {
+                        jQuery.fn.stickyNotes.editNote(this);
+                 });
+}
+
+var droppedNote = function(e, ui){
+  var pos = ui.draggable.position();
+
+  var pos_sticky = jQuery("#sticky-container").position();
+  var note = {
+      "id": new_note_id,
+      "text": jQuery("#p-note-" + new_note_id).html(),
+      "pos_x": pos.left - pos_sticky.left,
+      "pos_y": pos.top - pos_sticky.top,
+      "width": 80,
+      "height": 60
+  }
+  socket.emit("edited", {my: note});
+  ui.draggable.appendTo(this);
+}
+
+var edited = function(note) {
+  socket.emit('edited', {my: note});
+}
+var created = function(note) {
+  //socket.emit('created', {my: note});
+}
+
+var deleted = function(note) {
+  socket.emit('deleted', {my: note});
+}
+
+var moved = function(note) {
+  socket.emit('moved', {my: note});
+}
+
+var resized = function(note) {
+  socket.emit('resized', {my: note});
+}
+
+
+showppt = function(){
+  var c = jQuery(".current_note");
+  var id = "-1";
+  if ( c.size() != 0 ) {
+    id = c.attr("id").split("-")[1];
+  }
+
+  socket.emit('showme', {current: id});
+}
+
+jQuery(document).ready(function() {
+  var notes_arr = new Array();
+  jQuery(".comment").each(function(i, e){
+    var note = {};
+    note.id     = jQuery(e).find("p").eq(0).text();
+    note.text   = jQuery(e).find("p").eq(1).text();
+    note.pos_x  = parseInt(jQuery(e).find("p").eq(2).text());
+    note.pos_y  = parseInt(jQuery(e).find("p").eq(3).text());
+    note.width  = parseInt(jQuery(e).find("p").eq(4).text());
+    note.height = parseInt(jQuery(e).find("p").eq(5).text());
+    notes_arr.push(note);
+  });
+
+  var options = {
+     notes: notes_arr
+    ,resizable: true
+    ,controls: true
+    ,editCallback: edited
+    ,createCallback: created
+    ,deleteCallback: deleted
+    ,moveCallback: moved
+    ,resizeCallback: resized
+  };
+  jQuery("#notes").stickyNotes(options);
+
+  jQuery("#ppt").click(showppt);
+
+  jQuery("#new button.create").click(jQuery.fn.stickyNotes.tempNote);
+  jQuery("#new button.over").click(stopEdit);
+  jQuery("#sticky-container").droppable({
+         drop: droppedNote
+  });
+});
+
+socket.on('b_created', function(data){
+  var mydata = data.my;
+  var note = {};
+  note.id = mydata.id;
+  note.text = mydata.text;
+  note.pos_x = parseInt(mydata.pos_x);
+  note.pos_y = parseInt(mydata.pos_y);
+  note.width = parseInt(mydata.width);
+  note.height = parseInt(mydata.height);
+
+  jQuery.fn.stickyNotes.renderNote(mydata);
+});
+
+socket.on('b_edited', function(data){
+  var mydata = data.my;
+  var note = {};
+  var _edit = jQuery("#p-note-" + mydata.id);
+  if ( _edit.size() != 0 ) {
+    _edit.hide('clip', {}, 500, function(){
+                                  jQuery(this).html("<pre>" + mydata.text + "</pre>");
+                                  jQuery(this).show('clip', {}, 1000, function(){});
+                                });
+  } else {
+    note.id = mydata.id;
+    note.text = mydata.text;
+    note.pos_x = parseInt(mydata.pos_x);
+    note.pos_y = parseInt(mydata.pos_y);
+    note.width = parseInt(mydata.width);
+    note.height = parseInt(mydata.height);
+
+    jQuery.fn.stickyNotes.renderNote(mydata);
+    jQuery.fn.stickyNotes.notes.push(note);   
+  }
+});
+
+socket.on('b_moved', function(data){
+  var mydata = data.my;
+  jQuery("#note-" + mydata.id).animate({top: mydata.pos_y, left: mydata.pos_x});
+});
+
+socket.on('b_resized', function(data){
+  var mydata = data.my;
+  jQuery("#note-" + mydata.id).animate({width: mydata.width, height: mydata.height});
+});
+
+socket.on('b_deleted', function(data){
+  var mydata = data.my;
+  jQuery("#note-" + mydata.id).hide("explode", {}, 1000, function(){});
+});
+
+socket.on('next', function(data){
+  var current = data.current;
+  var next = data.next;
+  if ( current != null ) { //close and open
+    current1 = current[0];
+    jQuery(".current_note").animate({top: current1.pos_y,
+                                     left: current1.pos_x,
+                                     width: current1.width,
+                                     height: current1.height},
+                                     {complete: function(){
+                                       jQuery("#note-" + next.id).animate({top: 50, left: 50, width: 650, height: 650}).css({"z-index": "2000"}).addClass("current_note");
+                                       jQuery("#p-note-" + next.id).css({'font-size':'8ex'});
+                                     }})
+                           .removeClass("current_note")
+                           .css({'z-index':'1000'});;
+    jQuery("#p-note-" + current1.id).css({"z-index": "1000", 'font-size':'14px'})
+  } else {          //only open
+    jQuery("#note-" + next.id).animate({top: 50, left: 50, width: 650, height: 650})
+                              .css({"z-index": "2000"})
+                              .addClass("current_note");
+    jQuery("#p-note-" + next.id).css({'font-size':'8ex'});
+  }
+});
